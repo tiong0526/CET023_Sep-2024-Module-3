9c7e700891542202d8363d0a2aa1e0c8caa32c1c
tiong0526 <35533440+tiong0526@users.noreply.github.com>
2024-09-28 03:25:49 +0000

    1

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 from flask import Flask,render_template,request
+import google.generativeai as genai
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key="AIzaSyCFIL-2qRWHrUqzyf_TN3A5IKQsVgB2zHg")
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("index.html")) 
+    return(render_template("index.html"))
 
 @app.route("/prediction_DBS",methods=["GET","POST"])
 def prediction_DBS():
-    return(render_template("prediction_DBS.html")) 
+    return(render_template("prediction_DBS.html"))
 
 @app.route("/prediction_result_DBS",methods=["GET","POST"])
 def prediction_result_DBS():
     q = float(request.form.get("q"))
-    r = (-50.6*q) + 90.2
-    return(render_template("prediction_result_DBS.html",r=r)) 
+    r = (-50.6 * q) + 90.2
+    return(render_template("prediction_result_DBS.html",r=r))
 
 @app.route("/faq",methods=["GET","POST"])
 def faq():
-    return(render_template("faq.html")) 
+    return(render_template("faq.html"))
+
+@app.route("/q1",methods=["GET","POST"])
+def q1():
+    r = model.generate_content("How should I diversify my investment portfolio?")
+    return(render_template("q1_reply.html",r=r))
+
+@app.route("/q2",methods=["GET","POST"])
+def q2():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("q2_reply.html",r=r))
 
 if __name__ == "__main__":
     app.run()
